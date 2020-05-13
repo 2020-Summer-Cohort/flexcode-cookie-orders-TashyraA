1e101f96694f8aae8d2a6e7b2c51032a5efe404c
TashyraA <tashyra.adams@yahoo.com>
2020-05-13 14:32:48 -0400

    Final submission

--- src/main/java/cookies/CookieOrder.java
+++ src/main/java/cookies/CookieOrder.java
@@ -1,4 +1,21 @@
 package cookies;
 
 public class CookieOrder {
+
+    private int numBoxes;
+    private String variety;
+
+    //Two Pieces of Data
+    public CookieOrder(String localVariety, int localNumBoxes) {
+        this.numBoxes = localNumBoxes;
+        this.variety = localVariety;
+    }
+//Accessor Method
+    public int getNumBoxes() {
+        return numBoxes;
+    }
+//Accessor Method
+    public String getVariety() {
+        return this.variety;
+    }
 }
--- src/main/java/cookies/OrderApp.java
+++ src/main/java/cookies/OrderApp.java
@@ -1,7 +1,35 @@
 package cookies;
 
+import java.util.Scanner;
+
 public class OrderApp {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        MasterOrder myOrder = new MasterOrder();
+        myOrder.addOrder(new CookieOrder("TagaLongs", 1));
+        myOrder.addOrder(new CookieOrder("Thin Mints", 5));
+        myOrder.addOrder(new CookieOrder("Samoas", 2));
+        myOrder.addOrder(new CookieOrder("TagaLongs", 3));
+
+        System.out.println("Current order");
+        myOrder.showOrder();
+        System.out.println();
+        System.out.println("You have ordered " + myOrder.getTotalBoxes() + " boxes");
+        System.out.println();
+        System.out.println("What would you like to remove? (specify a variety or none)");
+
+        String varietyToRemove = input.nextLine();
+
+        System.out.println("You are removing " + myOrder.getVarietyBoxes(varietyToRemove) + " " + varietyToRemove);
+
+        myOrder.removeVariety(varietyToRemove);
+
+        System.out.println("Current order");
+
+        myOrder.showOrder();
+
 
     }
 }
